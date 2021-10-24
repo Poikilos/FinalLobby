4e49c1a6c5a853c6c99e212ac49d443e7945616f
poikilos <7557867+poikilos@users.noreply.github.com>
2021-10-24 14:24:56 -0400

    Add an install script.

---
+++ hasmodule.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Check for an installed module: hasmodule.py <module_name>
+The result is 0 if the module is installed, and non-zero if not (or if
+you don't specify a parameter)--therefore it is close to and possibly
+equivalent to: python -c "import <module_name>".
+"""
+import sys
+import subprocess
+import pkg_resources
+# See <https://stackoverflow.com/questions/44210656/how-to-check-if-a-
+# module-is-installed-in-python-and-if-not-install-it-within-t>
+
+if len(sys.argv) < 2:
+    exit(1)
+
+require = sys.argv[1].strip()
+
+if len(require) < 1:
+    exit(1)
+
+installed = {pkg.key for pkg in pkg_resources.working_set}
+
+if require not in installed:
+    # python = sys.executable
+    # subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+    exit(1)
+exit(0)
---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+KIVY_VENV="$HOME/venv/kivy"
+
+mkdir -p "$HOME/venv"
+
+R_TEXT="`pwd`/requirements.txt"
+#if [ ! -f "$R_TEXT" ]; then
+#    echo "* Error: \"$R_TEXT\" is missing."
+#    exit 1
+#fi
+
+setup_venv(){
+    echo "* setup_venv \"$KIVY_VENV\"..."
+    python -m venv "$KIVY_VENV"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+if [ ! -d "$KIVY_VENV" ]; then
+    setup_venv
+fi
+source "$KIVY_VENV/bin/activate"
+if [ $? -ne 0 ]; then
+    setup_venv
+    source "$KIVY_VENV/bin/activate"
+    if [ $? -ne 0 ]; then
+        echo "Error: Remaking $KIVY_VENV failed."
+        exit 1
+    fi
+fi
+echo "* using \"`which python`\""
+python -m pip install --upgrade pip setuptools virtualenv
+if [ $? -ne 0 ]; then
+    echo "* remaking $KIVY_VENV..."
+    deactivate
+    rm -Rf "$KIVY_VENV"
+    setup_venv
+    source "$KIVY_VENV/bin/activate"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+    python -m pip install --upgrade pip setuptools virtualenv
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+pip_or_end(){
+    if [ "@$1" = "@" ]; then
+        echo "Error: pip_or_end requires a package name as an argument."
+        exit 1
+    fi
+    python -m pip install --upgrade $1
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+#python -m pip install -r "$R_TEXT"
+
+pip_or_end constantly
+pip_or_end cython
+pip_or_end requests
+pip_or_end incremental
+pip_or_end twisted
+pip_or_end kivy
+pip_or_end zope.interface
+pip_or_end kivy-garden
+pip_or_end kivymd
+# Update requirements.txt:
+# > - Install packages with pip: `-r requirements.txt`
+# > - How to write configuration file `requirements.txt`
+# > - Export current environment configuration file: `pip freeze`
+# -<https://note.nkmk.me/en/python-pip-install-requirements/>
+which python
+deactivate
